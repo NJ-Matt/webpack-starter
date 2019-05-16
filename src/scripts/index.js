59f2c9663c040ccf2297977827c8125868c10bb7
Mathieu Bourg <mathieubourg@gmail.com>
2019-05-16 11:09:32 -0400

    Initial commit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,10 @@
-import '../styles/index.scss';
 
-console.log('webpack starterkit');
+function startCar(carId) {
+	let message = 'Starting...';
+	let startFn = function turnKey() {
+		console.log(message);
+	};
+	startFn();
+}
+
+startCar(123);
